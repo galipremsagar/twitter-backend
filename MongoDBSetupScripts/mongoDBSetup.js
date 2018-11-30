f80c87c008620a5759a53cb3bb2348e62ba1f0b4
galipremsagar <sagarprem75@gmail.com>
2018-11-30 18:25:22 -0500

    Code refactoring & removing unnecessary files.

--- MongoDBSetupScripts/mongoDBSetup.js
+++ MongoDBSetupScripts/mongoDBSetup.js
@@ -27,13 +27,13 @@
 // });
 //
 //
-// // MongoClient.connect(url, function(err, db) {
-// //     if (err) throw err;
-// //     var dbo = db.db("mydb");
-// //
-// //     dbo.collection("customers").insertOne(myobj, function(err, res) {
-// //         if (err) throw err;
-// //         console.log("1 document inserted");
-// //         db.close();
-// //     });
-// // });
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//
+//     dbo.collection("customers").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("1 document inserted");
+//         db.close();
+//     });
+// });
--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var signUpRouter = require('./routes/signup');
 var loginRouter = require('./routes/login');
 
@@ -21,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/signup', signUpRouter);
 app.use('/login', loginRouter);
 
